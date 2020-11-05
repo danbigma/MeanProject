49b16935d391cdf07138a1ecfbfc87acbb287b68
danbigma <danylo.bigma@everis.com>
2020-11-05 15:52:24 +0100

    from work

--- routes/category.js
+++ routes/category.js
@@ -2,7 +2,12 @@ const express = require("express");
 const controller = require("../controllers/category");
 const router = express.Router();
 
-//localhost:5000/api/analytics/overview
+//localhost:5000/api/category
 router.get("/", controller.getAll);
+router.get("/:id", controller.getById);
+router.post("/", controller.create);
+router.patch("/:id", controller.update);
+router.delete("/:id", controller.remove);
+
 
 module.exports = router;
