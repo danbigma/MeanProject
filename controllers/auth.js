7254e01b402e4bea50494b6f0f9f293e554d9618
db90 <daniil.bigma@gmail.com>
2020-11-05 22:25:14 +0100

    sing in is done

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
 module.exports.login = function (req, res) {
     res.status(200).json(
         {
@@ -9,10 +11,26 @@ module.exports.login = function (req, res) {
     )
 };
 
-module.exports.register = function (req, res) {
-    res.status(200).json(
-        {
-            register: true
+module.exports.register = async function (req, res) {
+    const candidate = await User.findOne({ email: req.body.email });
+
+    if (candidate) {
+        res.status(409).json({
+            message: "Email exist!"
+        });
+    } else {
+        const salt = bcrypt.genSaltSync(10);
+        const password = req.body.password;
+        const user = new User({
+            email: req.body.email,
+            password: bcrypt.hashSync(password, salt)
+        });
+        
+        try {
+            await user.save();
+            res.status(201).json(user);
+        } catch (e) {
+            
         }
-    )
+    }    
 };
