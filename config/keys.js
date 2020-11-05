1abe6e0963846b49384f3af2721dab46ce27ef2e
db90 <daniil.bigma@gmail.com>
2020-11-05 22:56:30 +0100

    login is done!

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://danbigma:AOIVqyVS5s1C6gyD@cluster0.ojxnq.mongodb.net/MEAN_BD?retryWrites=true&w=majority"
+    mongoURI: "mongodb+srv://danbigma:AOIVqyVS5s1C6gyD@cluster0.ojxnq.mongodb.net/MEAN_BD?retryWrites=true&w=majority",
+    jwt: "dev-jwt" 
 }
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,34 @@
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../config/keys");
 const User = require("../models/User");
-module.exports.login = function (req, res) {
-    res.status(200).json(
-        {
-            login: {
-                email: req.body.email,
-                password: req.body.password
-            }
+
+module.exports.login = async function (req, res) {
+    const candidate = await User.findOne({
+        email: req.body.email
+    });
+    
+    if (candidate) {
+        const passwordResult = bcrypt.compareSync(req.body.password, candidate.password);
+        if (passwordResult) {
+            const token = jwt.sign({
+                email: candidate.email,
+                userId: candidate._id
+            }, keys.jwt, {expiresIn: 60 * 60});
+            
+            res.status(200).json({
+                token: "Bearer " + token
+            });
+        } else {
+            res.status(401).json({
+                message: "Пароли не совпадают"
+            })
         }
-    )
+    } else {
+        res.status(401).json({
+            message: "Юзер с таким email не найден."
+        });
+    }
 };
 
 module.exports.register = async function (req, res) {
