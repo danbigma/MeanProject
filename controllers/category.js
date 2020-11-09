1b71c4ac68b0ed82e9ba2e4a06faa88e7c982297
db90 <daniil.bigma@gmail.com>
2020-11-09 22:30:40 +0100

    category is done

--- controllers/category.js
+++ controllers/category.js
@@ -1,26 +1,60 @@
-module.exports.getAll = function(req, res) {
-    res.status(200).json({
-        masage: "/controllers/category",
-        method: "getAll"
-    });
+const Category = require("../models/Category");
+const Position = require("../models/Position");
+const errorHandler = require("../utils/errorHandler");
+
+module.exports.getAll = async function (req, res) {
+    try {
+        const categories = Category.find({ 
+            user: req.user.id
+        });
+        res.status(200).json(categories);
+    } catch (e) {
+        errorHandler(res, e);
+    }
 };
 
-module.exports.getById = function(req, res) {
-    res.status(200).json({
-        masage: "/controllers/category",
-        method: "getById" 
-    });
+module.exports.getById = async function (req, res) {
+    try {
+        const category = Category.findById({category: req.params.id});
+        res.status(200).json(category);
+    } catch (e) {
+        errorHandler(res, e);
+    }
 };
 
-module.exports.remove = function(req, res) {
-    
+module.exports.remove = async function (req, res) {
+    try {
+        await Category.remove({_id: req.params.id});
+        await Position.remove({category: req.params.id});
+        res.status(200).json({
+            message: "Позиция была удалена."
+        });
+    } catch (e) {
+        errorHandler(res, e); 
+    }
 };
 
-module.exports.create = function(req, res) {
-    
+module.exports.create = async function (req, res) {
+    try {
+        const category = Category.find({
+            category: req.params.categoryId,
+            user: req.user.id
+        });
+        res.status(200).json(category);
+    } catch (e) {
+        errorHandler(res, e);
+    }
 };
 
-module.exports.update = function(req, res) {
-    
+module.exports.update = async function (req, res) {
+    try {
+        const category = Category.find({
+            category: req.params.categoryId,
+            user: req.user.id
+        });
+        res.status(200).json(category);
+    } catch (e) {
+        errorHandler(res, e);
+    }
 };
 
