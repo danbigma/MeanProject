e02cce075fa33349327e8b5d15ec9b6a4fa0cc58
danbigma <daniil.bigma@gmail.com>
2020-11-14 21:59:36 +0100

    add functionaity on category

--- controllers/category.js
+++ controllers/category.js
@@ -1,6 +1,7 @@
 const Category = require("../models/Category");
 const Position = require("../models/Position");
 const errorHandler = require("../utils/errorHandler");
+const { update } = require("./position");
 
 module.exports.getAll = async function (req, res) {
     try {
@@ -37,21 +38,32 @@ module.exports.remove = async function (req, res) {
 module.exports.create = async function (req, res) {
     try {
         const category = Category.find({
-            category: req.params.categoryId,
-            user: req.user.id
+            name: req.body.name,
+            user: req.user.id,
+            imageSrc:  req.file ? req.file.path : ""
         });
-        res.status(200).json(category);
+        await category.save();
+        res.status(201).json(category);
     } catch (e) {
         errorHandler(res, e);
     }
 };
 
 module.exports.update = async function (req, res) {
+    const updated = {
+        name: req.body.name
+    };
+    
+    if (req.file) {
+        updated.imageSrc = req.file.path;
+    }
+    
     try {
-        const category = Category.find({
-            category: req.params.categoryId,
-            user: req.user.id
-        });
+        const category = await Category.findOneAndUpdate(
+            {_id: req.params.id},
+            {$set: updated},
+            {new: true}
+        );
         res.status(200).json(category);
     } catch (e) {
         errorHandler(res, e);
--- routes/category.js
+++ routes/category.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const passport = require("passport");
-const controller = require("../controllers/category");
+const upload = require("../controllers/category");
+const controller = require("../middleware/upload");
+
 const router = express.Router();
 
 // localhost:5000/api/category
 router.get("/", passport.authenticate("jwt", { session: false }), controller.getAll);
-router.get("/:id", controller.getById);
-router.post("/", controller.create);
-router.patch("/:id", controller.update);
-router.delete("/:id", controller.remove);
+router.get("/:id", passport.authenticate("jwt", { session: false }), controller.getById);
+router.post("/", passport.authenticate("jwt", { session: false }), upload.single("image"), controller.create);
+router.patch("/:id", passport.authenticate("jwt", { session: false }), upload.single("image"), controller.update);
+router.delete("/:id", passport.authenticate("jwt", { session: false }), controller.remove);
 
 
 module.exports = router;
