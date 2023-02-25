b23199ce7cba1a3dd677d704d00eaea7b3ac7f34
danbigma <danylo.bigma@gmail.com>
2023-02-25 23:09:41 +0100

    add angular

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-const port = process.env.PORT || 4200;
+const port = process.env.PORT || 8080;
 
 app.listen(port, function() {
     console.log("Server has been started on " + port + " port");
