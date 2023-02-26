6b939be2c3426a02695a3c078fa9bec24d352d7e
danbigma <danylo.bigma@gmail.com>
2023-02-26 01:16:44 +0100

    add login page and some ts

---
+++ client/src/app/login-page/login-page.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css'],
+})
+export class LoginPageComponent {
+  form!: FormGroup;
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(4),
+      ]),
+    });
+  }
+  
+  onSubmit() {
+
+  }
+}
