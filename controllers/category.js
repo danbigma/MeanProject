7c1cfd4cfe8f700468a8b2df2cff6c9b1d04ba9d
danbigma <danbigma@gmail.com>
2024-01-04 00:49:59 +0100

    todavia mejor

--- controllers/category.js
+++ controllers/category.js
@@ -6,70 +6,56 @@ const HTTP_STATUS_OK = 200;
 const HTTP_STATUS_CREATED = 201;
 const HTTP_STATUS_NOT_FOUND = 404;
 
-const handleErrors = async (res, func) => {
+const handleErrors = (func) => async (req, res) => {
   try {
-    await func();
+    await func(req, res);
   } catch (error) {
     errorHandler(res, error);
   }
 };
 
-module.exports.getAll = async function (req, res) {
-  handleErrors(res, async () => {
-    const categories = await Category.find({ user: req.user.id });
-    res.status(HTTP_STATUS_OK).json(categories);
-  });
-};
+module.exports.getAll = handleErrors(async (req, res) => {
+  const categories = await Category.find({ user: req.user.id });
+  res.status(HTTP_STATUS_OK).json(categories);
+});
 
-module.exports.getById = async function (req, res) {
-  handleErrors(res, async () => {
-    const category = await Category.findById(req.params.id);
-    if (!category) {
-      res.status(HTTP_STATUS_NOT_FOUND).json({ message: "Categoría no encontrada." });
-    } else {
-      res.status(HTTP_STATUS_OK).json(category);
-    }
-  });
-};
+module.exports.getById = handleErrors(async (req, res) => {
+  const category = await Category.findById(req.params.id);
+  if (!category) {
+    return res
+      .status(HTTP_STATUS_NOT_FOUND)
+      .json({ message: "Categoría no encontrada." });
+  }
+  res.status(HTTP_STATUS_OK).json(category);
+});
 
-module.exports.remove = async function (req, res) {
-  handleErrors(res, async () => {
-    await Category.remove({ _id: req.params.id });
-    await Position.remove({ category: req.params.id });
-    res.status(HTTP_STATUS_OK).json({
-      message: "Categoría eliminada.",
-    });
-  });
-};
+module.exports.remove = handleErrors(async (req, res) => {
+  await Category.remove({ _id: req.params.id });
+  await Position.remove({ category: req.params.id });
+  res.status(HTTP_STATUS_OK).json({ message: "Categoría eliminada." });
+});
 
-module.exports.create = async function (req, res) {
+module.exports.create = handleErrors(async (req, res) => {
   const category = new Category({
     name: req.body.name,
     user: req.user.id,
     imageSrc: req.file ? req.file.path : "",
   });
 
-  handleErrors(res, async () => {
-    await category.save();
-    res.status(HTTP_STATUS_CREATED).json(category);
-  });
-};
+  await category.save();
+  res.status(HTTP_STATUS_CREATED).json(category);
+});
 
-module.exports.update = async function (req, res) {
-  const updated = {
-    name: req.body.name,
-  };
-
+module.exports.update = handleErrors(async (req, res) => {
+  const updated = { name: req.body.name };
   if (req.file) {
     updated.imageSrc = req.file.path;
   }
 
-  handleErrors(res, async () => {
-    const category = await Category.findOneAndUpdate(
-      { _id: req.params.id },
-      { $set: updated },
-      { new: true }
-    );
-    res.status(HTTP_STATUS_OK).json(category);
-  });
-};
+  const category = await Category.findOneAndUpdate(
+    { _id: req.params.id },
+    { $set: updated },
+    { new: true }
+  );
+  res.status(HTTP_STATUS_OK).json(category);
+});
